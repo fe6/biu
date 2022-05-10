eb0a56494b4a131782ee65824929e9bf60fc97e3
天意 <649076408@qq.com>
2022-05-10 23:02:36 +0800

    fix($deps): remove esbuild for index

--- packages/deps/src/index.ts
+++ packages/deps/src/index.ts
@@ -3,11 +3,10 @@
 // ? deps
 import yParser from '../compiled/yargs-parser';
 import chalk from '../compiled/chalk';
-import esbuild from '../compiled/esbuild';
 import crossSpawn from '../compiled/cross-spawn';
 import fsExtra from '../compiled/fs-extra';
 export * from '../compiled/commander';
 import reactRefreshWebpack from '@pmmmwh/react-refresh-webpack-plugin';
 // ? deps
 
-export { yParser, chalk, crossSpawn, esbuild, fsExtra, reactRefreshWebpack };
+export { yParser, chalk, crossSpawn, fsExtra, reactRefreshWebpack };
